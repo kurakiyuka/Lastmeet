e2289c723eff705c6f3a54d1b014cf2998032e3a
kurakiyuka@home <myzlovebqb@sina.com>
2015-11-17 01:39:36 +0800

    15.11.17@home
    begin tasks

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Last Time You Meet</title>
+    <link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
+    <script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+<div class="container">
+    <nav class="navbar navbar-default">
+        <!-- Navbar Contents -->
+    </nav>
+</div>
+
+@yield('content')
+</body>
+</html>
